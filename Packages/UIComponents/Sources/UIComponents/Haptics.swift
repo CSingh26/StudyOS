5eec2f6e4d83a211f7153a15cab7ff1f1bb66e4e
Chaitanya Singh <157046183+CSingh26@users.noreply.github.com>
2026-01-24 20:30:51 -0700

    chore(haptics): gate simulator feedback

--- Packages/UIComponents/Sources/UIComponents/Haptics.swift
+++ Packages/UIComponents/Sources/UIComponents/Haptics.swift
@@ -1,15 +1,27 @@
+import CoreHaptics
 import UIKit
 
 public enum Haptics {
+    private static var isHapticsAvailable: Bool {
+        #if targetEnvironment(simulator)
+        return false
+        #else
+        if #available(iOS 13.0, *) {
+            return CHHapticEngine.capabilitiesForHardware().supportsHaptics
+        }
+        return true
+        #endif
+    }
+
     public static func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
-        guard !UIAccessibility.isReduceMotionEnabled else { return }
+        guard !UIAccessibility.isReduceMotionEnabled, isHapticsAvailable else { return }
         let generator = UIImpactFeedbackGenerator(style: style)
         generator.prepare()
         generator.impactOccurred()
     }
 
     public static func notification(_ type: UINotificationFeedbackGenerator.FeedbackType) {
-        guard !UIAccessibility.isReduceMotionEnabled else { return }
+        guard !UIAccessibility.isReduceMotionEnabled, isHapticsAvailable else { return }
         let generator = UINotificationFeedbackGenerator()
         generator.prepare()
         generator.notificationOccurred(type)
